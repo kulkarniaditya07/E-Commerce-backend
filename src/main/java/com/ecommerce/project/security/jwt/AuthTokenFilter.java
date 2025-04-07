48faabf02fdca6654bc849a93fae5517113ed5b2
kulkarniaditya07 <kulkarniaditya074@gmail.com>
2025-04-07 16:14:26 +0530

    AuthTokenFilter in progress

---
+++ src/main/java/com/ecommerce/project/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain){
+
+
+
+    }
+}
