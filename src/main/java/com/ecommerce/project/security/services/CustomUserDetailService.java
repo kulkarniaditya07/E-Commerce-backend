1fbe9d775ee8568eb16c0a211e36e4a4518e72b1
kulkarniaditya07 <kulkarniaditya074@gmail.com>
2025-04-07 17:46:16 +0530

    returns custom UserDetailsService

--- src/main/java/com/ecommerce/project/security/services/CustomUserDetailService.java
+++ src/main/java/com/ecommerce/project/security/services/CustomUserDetailService.java
@@ -1,4 +1,28 @@
 package com.ecommerce.project.security.services;
 
-public class CustomUserDetailSevice {
+import com.ecommerce.project.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByUsername(username)
+                        .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+            }
+        };
+    }
+
 }
