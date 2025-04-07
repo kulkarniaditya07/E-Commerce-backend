fe1e62ee97f2aace08c37f9bdfda4d33bf7f34db
kulkarniaditya07 <kulkarniaditya074@gmail.com>
2025-04-07 17:44:49 +0530

    yet to work on this

--- src/main/java/com/ecommerce/project/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/SecurityConfig.java
@@ -1,4 +1,15 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
 }
