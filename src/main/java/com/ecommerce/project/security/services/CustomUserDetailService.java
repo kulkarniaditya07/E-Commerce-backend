4660bd85b618e4cfcc2b2972faccae9e988382ce
kulkarniaditya07 <kulkarniaditya074@gmail.com>
2025-04-08 19:12:35 +0530

    custom userDetails Service

--- src/main/java/com/ecommerce/project/security/services/CustomUserDetailService.java
+++ src/main/java/com/ecommerce/project/security/services/CustomUserDetailService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ public class CustomUserDetailService {
         return new UserDetailsService() {
 
             @Override
+            @Transactional
             public UserDetails loadUserByUsername(String username) {
                 return userRepository.findByUsername(username)
                         .orElseThrow(()-> new UsernameNotFoundException("User not found"));
