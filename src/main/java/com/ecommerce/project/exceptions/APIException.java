443f63928e346c11a10c07321a51578a787777dc
kulkarniaditya07 <kulkarniaditya074@gmail.com>
2025-04-17 21:29:37 +0530

    No major change

--- src/main/java/com/ecommerce/project/exceptions/APIException.java
+++ src/main/java/com/ecommerce/project/exceptions/APIException.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.exceptions;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.io.Serial;
@@ -8,23 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class APIException extends RuntimeException {
 
     private  HttpStatus httpStatus;
-    private final List<String> errorMessage=new ArrayList<>();
+    private String message;
     @Serial
     private static final long serialVersionUID=1L;
 
-    public APIException() {
-    }
-
     public APIException(String message) {
-        super(message);
+        this.message = message;
     }
 
-    public APIException(String message, HttpStatus status) {
-        super(message);
-        this.httpStatus=status;
-        this.errorMessage.add(message);
+    public APIException(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
     }
 }
